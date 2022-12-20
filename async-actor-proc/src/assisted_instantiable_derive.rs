c1256bc321c3d760ee28be8584b967bd3b5848d3
jan <contact@jan-br.de>
2022-12-20 18:07:12 +0100

    Fix macro error

--- async-actor-proc/src/assisted_instantiable_derive.rs
+++ async-actor-proc/src/assisted_instantiable_derive.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use syn::{Item, ItemImpl, ItemStruct, Result};
 use quote::{format_ident, quote};
 use proc_macro2::{TokenStream as TokenStream2};
-use crate::util::{format_injectable_struct_instantiation, format_generic_constraints, format_generic_definition, format_generics_as_tuple, format_handle_name, format_instantiation_data_name, format_name, find_non_injectable_non_default_fields};
+use crate::util::{format_injectable_struct_instantiation, format_generic_constraints, format_generic_definition, format_generics_as_tuple, format_handle_name, format_instantiation_data_name, format_name, find_non_injectable_non_default_fields, format_generic_usage};
 
 pub fn assisted_instantiable_derive(input: TokenStream) -> TokenStream {
   let input = TokenStream2::from(input);
@@ -14,13 +14,13 @@ pub fn assisted_instantiable_derive(input: TokenStream) -> TokenStream {
 }
 
 fn parse_and_expand(input: TokenStream2) -> Result<TokenStream2> {
-  let item = parse(input.clone())?;
+  let item = parse(input)?;
   expand(&item)
 }
 
 
 fn parse(input: TokenStream2) -> Result<ItemStruct> {
-  Ok(syn::parse2(input)?)
+  syn::parse2(input)
 }
 
 fn expand(mut original: &ItemStruct) -> Result<TokenStream2> {
@@ -70,6 +70,7 @@ fn create_assisted_instantiable_implementation(original: &ItemStruct) -> Result<
   let original_name = format_name(&original.ident);
   let instantiation_data_name = format_instantiation_data_name(&original.ident);
   let generic_definition = format_generic_definition(&original.generics);
+  let generic_usage = format_generic_usage(&original.generics);
   let generic_constraints = format_generic_constraints(&original.generics);
   let non_injectable_non_default_fields: Vec<_> = find_non_injectable_non_default_fields(&original).iter().map(|field| {
     let name = field.ident.clone().unwrap();
@@ -77,14 +78,23 @@ fn create_assisted_instantiable_implementation(original: &ItemStruct) -> Result<
   }).collect();
   let instantiation = format_injectable_struct_instantiation(&original, &quote! {Self}, true, Some(non_injectable_non_default_fields.clone()));
 
-  syn::parse2(quote! {
+
+  let instantiation_data_destructor = if non_injectable_non_default_fields.is_empty() {
+    quote!()
+  }else{
+    quote!(let #instantiation_data_name #generic_usage { #(#non_injectable_non_default_fields,)* .. } = data;)
+  };
+
+  let x = quote! {
     #[async_trait::async_trait]
-    impl async_actor::inject::assisted_inject::AssistedInstantiable<#instantiation_data_name #generic_definition> for #original_name #generic_constraints {
-      async fn instantiate(injector: async_actor::inject::InjectorHandle, data: #instantiation_data_name #generic_definition) -> #original_name{
-        let #instantiation_data_name #generic_definition { #(#non_injectable_non_default_fields,)* .. } = data;
+    impl #generic_definition async_actor::inject::assisted_inject::AssistedInstantiable<#instantiation_data_name #generic_definition> for #original_name #generic_definition #generic_constraints {
+      async fn instantiate(injector: async_actor::inject::InjectorHandle, data: #instantiation_data_name #generic_definition) -> Self {
+        #instantiation_data_destructor
 
         #instantiation
       }
     }
-  })
+  };
+  println!("{}", x);
+  syn::parse2(x)
 }
