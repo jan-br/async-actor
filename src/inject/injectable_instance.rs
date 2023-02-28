8edb7eee4643e6bdcdc775ac752f6a77ed611f58
jan <contact@jan-br.de>
2023-02-28 03:33:14 +0100

    Add manually injectable instance

--- src/inject/injectable_instance.rs
+++ src/inject/injectable_instance.rs
@@ -8,3 +8,14 @@ pub trait InjectableInstance where Self: Any + Send + Sync {
   type Inner: HasHandleWrapper<HandleWrapper=Self>;
   fn create_instance(injector: Injector) -> Pin<Box<dyn Future<Output=Box<Self>> + Send + Sync>>;
 }
+
+pub trait ManuallyInjectableInstance where Self: Any + Send + Sync {
+  type Inner: HasHandleWrapper<HandleWrapper=Self>;
+}
+impl<T: ManuallyInjectableInstance> InjectableInstance for T {
+  type Inner = T::Inner;
+
+  fn create_instance(injector: Injector) -> Pin<Box<dyn Future<Output=Box<Self>> + Send + Sync>> {
+    panic!("This struct cannot be injected before it was manually bound.")
+  }
+}
