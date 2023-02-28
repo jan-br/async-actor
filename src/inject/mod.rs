e750c5794d37dabd6d46eff1aced515f52213a4a
jan <contact@jan-br.de>
2023-02-28 03:44:13 +0100

    Fix injector bind_value function

--- src/inject/mod.rs
+++ src/inject/mod.rs
@@ -89,10 +89,20 @@ impl Injector {
     self.inner.write().await.mappings.insert(TypeId::of::<T>(), TypeId::of::<I>());
   }
 
-  pub async fn bind_value<'a, T>(&'a self, value: T) -> Pin<Box<dyn Future<Output=()> + Send + Sync + 'a>> where T: Send + Sync + 'static {
+  pub async fn bind_value<'a, C>(&'a self, value: C::HandleWrapper) -> Pin<Box<dyn Future<Output=()> + Send + Sync + 'a>>
+    where
+      C: HasHandleWrapper + Send + Sync + 'static,
+      C::HandleWrapper: InjectableInstance<Inner=C>,
+  {
     Box::pin(async move {
       let mut inner = self.inner.write().await;
-      inner.injected_instances.insert(Binding::Unnamed(TypeId::of::<T>()), Arc::new(value));
+      let lazy_cell: Arc<LazyCell<C::HandleWrapper>> = Arc::new(LazyCell::new({
+        async move {
+          value
+        }
+      }));
+      lazy_cell.get().await;
+      inner.injected_instances.insert(Binding::Unnamed(TypeId::of::<C>()), lazy_cell);
     })
   }
 
